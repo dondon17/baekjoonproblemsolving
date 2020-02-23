0688116e8fc49583c83662c8f71e527b7bcd4080
dondon <qwerqw889@ajou.ac.kr>
2020-02-23 13:52:59 +0900

    2675 try

--- 2675.c
+++ 2675.c
@@ -1,16 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 #pragma warning(disable:4996)
 int main(void){
     int num;
+    int len;
+    char tmp[50];
 
     scanf("%d", &num);
 
-    int *n = (int*)malloc(sizeof(int)*num);
-    char **word = (char*)malloc(sizeof(char)*num); 
+    char *word[num];
 
     for(int i=0;i<num;i++){
-        scanf("%s", word[i]);
+        scanf("%s", tmp);
+        len = strlen(tmp)+1;
+
+        word[i] = (char*)malloc(sizeof(char)*len);
     }
     for (int i = 0; i < num; i++){
         printf("%s\n", word[i]);
