81af0304bfb7a812281e01def2c2f79bace41e5d
dondon17 <qwerqw889@ajou.ac.kr>
2020-03-13 21:05:33 +0900

    에라토스테네스 솔직히 뭐가 다른지 모르겠다..

--- 1929.c
+++ 1929.c
@@ -1,18 +1,28 @@
 #include<stdio.h>
+#include<math.h>
 #pragma warning(disable:4996)
-int num[1000000];
+int num[1000000]={0, };
+
+void getPrime(int start, int last){
+    for(int i = 1; i <= last; i++){
+        if(i == 1) num[i] = 0;
+        else num[i] = i;
+    }
+
+    int sqrLast = (int)sqrt(last);
+    for(int i = 0; i <= sqrLast; i++){
+        if(num[i] == 0) continue;
+        for(int j = i*2; j <= last; j+=i) num[j] = 0;
+    }
+
+    for(int i = start; i <= last; i++){
+        if(num[i] != 0) printf("%d\n", num[i]);
+    }
+}    
 int main(void){
     
     int start, last;
     scanf("%d %d", &start, &last);
-
-    for(int i = 2; i <= last; i++){
-        if(!num[i]){
-            for(int j = i+1; j*2 < last; j+=i){
-                num[j] = 1;
-            }
-        }
-        if(i >= start && num[i] == 0) printf("%d\n", i);
-    }
+    getPrime(start, last);
     return 0;
 }
