5cda5404062479cc232a6f432a2960ec3248da53
dondon17 <qwerqw889@ajou.ac.kr>
2020-03-15 15:32:47 +0900

    stack 개념을 이용해 계산

--- 10773.c
+++ 10773.c
@@ -1,64 +1,24 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 #pragma warning(disable:4996)
-#define TRUE 1
-#define FALSE 0
-#define MINUS -1
-#define MAX_SIZE 100000
-
-typedef struct _stack{
-    int arr[MAX_SIZE];
-    int top;
-}Stack;
-
-void init_Stack(Stack *sp){
-    sp->top = -1;
-}
-
-int is_Empty(Stack *sp){
-    if(sp->top < -1) return TRUE;
-    else return FALSE;
-}
-
-int is_Full(Stack *sp){
-    if(sp->top+1 >= MAX_SIZE) return TRUE;
-    else return FALSE;
-}
-
-int Pop(Stack *sp){
-    if(is_Empty(sp) == TRUE) return TRUE;
-    else return sp->arr[(sp->top)--];
-}
-
-void Push(Stack *sp, int data){
-    if(is_Full(sp) == TRUE) return;
-    else sp->arr[++(sp->top)] = data;
-}
-
-int Peek(Stack *sp){
-    if(sp->top == -1) return -1;
-    else return sp->arr[sp->top];
-}
 
 int main(void){
-    int k, num, sum = 0;
-    Stack stack;
-    init_Stack(&stack);
+    int inputarr[10001], resultarr[10001];
+    int testcase, num, sum = 0;
+    int top = 0;
 
-    scanf("%d", &k);
+    scanf("%d", &testcase);
+    int* list = (int*)malloc(sizeof(int)*testcase);
 
-    for (int i = 0; i < k; i++){
+    for(int i = 0; i < testcase; i++){
         scanf("%d", &num);
-
-        if(num == 0) {
-            Pop(&stack);
-            printf("%d-----\n", Peek(&stack));
-        }
-        else {
-            Push(&stack, num);
-            printf("%d+++++\n", Peek(&stack));
-        }
+        if(num != 0) list[top++] = num;
+        else top--;
     }
-    printf("%d\n", num);
+    
+    for(int i = 0; i < top; i++) sum+=list[i];
+
+    printf("%d\n", sum);
     return 0;
 }
