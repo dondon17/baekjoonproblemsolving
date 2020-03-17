d79e0a694171ca41d46163c11153d673b64cb191
dondon17 <qwerqw889@ajou.ac.kr>
2020-03-17 23:14:34 +0900

    더하기 사이클 구하기

---
+++ 1110.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+int getnum(int n){
+    return (n%10)*10+(((n/10)+(n%10))%10);
+}
+int main(void){
+    int n, count = 0;
+    scanf("%d", &n);
+    int new = getnum(n);
+    
+    while (1){
+        
+        if(n == new){
+            printf("%d\n", ++count);
+            break;
+        }
+        else{
+            new = getnum(new);
+            count++;
+        }
+    }
+
+    return 0;
+}
