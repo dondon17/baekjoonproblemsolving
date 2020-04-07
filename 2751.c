9c8d035ca3ef6b7c1084108a1aad6a12a850fa01
dondon17 <qwerqw889@ajou.ac.kr>
2020-04-07 16:14:33 +0900

    정렬 2

--- 2751.c
+++ 2751.c
@@ -3,21 +3,30 @@
 #pragma warning(disable:4996)
 
 int compare(const void *a, const void *b){
-    if(*(int*)a > *(int*)b) return 1;
-    else if(*(int*)a < *(int*)b) return -1;
+    int num1 = *(int*)a;
+    int num2 = *(int*)b;
+
+    if(num1 < num2) return -1;
+    if(num1 > num2) return 1;
     else return 0;
 }
 int main(void){
     int n, temp;
     scanf("%d", &n);
 
+    // 길이가 n인 정수형 배열 생성
     int* arr = (int*)malloc(sizeof(int)*n);
+    // 배열 요소 입력
     for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+    // 배열 정렬
     qsort(arr, n, sizeof(int), compare);
-
-
+    // 정렬된 배열 출력
     for(int i=0; i<n; i++) printf("%d\n", arr[i]);
+    // 메모리 해제
+    free(arr);
     return 0;
 }
 
-// 참고: 공대생의 컴공생활(evga7.tistory.com/50)
+// void qsort(void *base, size_t NumOfElements, size_t SizeOfElements, int compare)
+// qsort(정렬할 배열, 요소 개수, 요소 사이즈, 비교함수)
+// compare만 작성해주면 끝
